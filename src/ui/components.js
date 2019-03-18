e27af5c95a526fbab604a9c3af848e80008d80a6
Jaskaran Singh <jaskaransingh@Jaskarans-iMac.local>
2019-03-17 20:36:31 -0500

    ui tweaks

--- src/ui/components.js
+++ src/ui/components.js
@@ -12,14 +12,14 @@ const Component = (props) => {
         <div>
             <Heading title="Basic Information"/>
             <Weather location={data2} weather={data3}/>
-            <div className = "row">
-                <div className = "col">
+            <div className="row">
+                <div className = "col-xl">
                     <Heading title="Map Visualization"/>
                     <div className = "shadow p-3 mb-5 bg-white rounded">
                         <Map lat={data[data.length - 1].latitude} lng={data[data.length - 1].longitude}/>
                     </div>
                 </div>
-                <div className = "col">
+                <div className="col-sm">
                     <Heading title="Graph Visualization"/>
                     <div className="shadow p-3 mb-5 bg-white rounded">
                         <Graph data={data}/>
